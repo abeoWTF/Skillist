8e3c1c08a23aba431de4cd5f7cd380f28468dd35
Jari Sadinm�ki <abeo@live.se>
2017-10-29 01:04:56 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Skillist
+{
+    
+    public partial class MainWindow : Window
+    {
+
+        List<User> users = new List<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //CanSwedishE = canSwedishE;
+        //CanSwedishD = canSwedishD;
+        //CanGermanE = canGermanE;
+        //CanGermanD = canGermanD;
+        //CanNorwegianE = canNorwegianE;
+        //CanNorwegianD = canNorwegianD;
+        //CanReturnPost = canReturnPost;
+        //CanClubApplication = canClubApplication;
+        //CanClubApplication = canClubChangeHere;
+
+        
+
+        private void name_TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            name_TextBox.Clear();
+            name_TextBox.Opacity = 100;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool swedishEQ, swedishDQ, germanEQ, germanDQ, norwegianEQ, norwegianDQ, returnP, clubAP, clubCH;
+
+            if (Equeue_Checkbox.IsChecked == true) { swedishEQ = true; }
+            else { swedishEQ = false;}
+            if (Dqueue_Checkbox.IsChecked == true) { swedishDQ = true; }
+            else { swedishDQ = false; }
+            if (Equeue_De_Checkbox.IsChecked == true) { germanEQ = true; }
+            else { germanEQ = false; }
+            if (Dqueue_De_Checkbox.IsChecked == true) { germanDQ = true; }
+            else { germanDQ = false; }
+            if (Equeue_No_Checkbox.IsChecked == true) { norwegianEQ = true; }
+            else { norwegianEQ = false; }
+            if (Dqueue_No_Checkbox.IsChecked == true) { norwegianDQ = true; }
+            else { norwegianDQ = false; }
+            if (Returpost_checkbox.IsChecked == true) { returnP = true; }
+            else { returnP = false; }
+            if (Club_Checkbox.IsChecked == true) { clubAP = true; }
+            else { clubAP = false; }
+            if (ClubChangeHere_Checkbox.IsChecked == true) { clubCH = true; }
+            else { clubCH = false; }
+
+
+
+            //users.Add(new User(name_TextBox.Text, eq));
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillist
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public bool CanSwedishE { get; set; }
+        public bool CanSwedishD { get; set; }
+        public bool CanGermanE { get; set; }
+        public bool CanGermanD { get; set; }
+        public bool CanNorwegianE { get; set; }
+        public bool CanNorwegianD { get; set; }
+        public bool CanReturnPost { get; set; }
+        public bool CanClubApplication { get; set; }
+        public bool CanClubChangeHere { get; set; }
+
+        public User(string name, bool canSwedishE, bool canSwedishD, bool canGermanE, bool canGermanD, bool canNorwegianE, bool canNorwegianD, bool canReturnPost, bool canClubApplication, bool canClubChangeHere)
+        {
+            UserName = name;
+            CanSwedishE = canSwedishE;
+            CanSwedishD = canSwedishD;
+            CanGermanE = canGermanE;
+            CanGermanD = canGermanD;
+            CanNorwegianE = canNorwegianE;
+            CanNorwegianD = canNorwegianD;
+            CanReturnPost = canReturnPost;
+            CanClubApplication = canClubApplication;
+            CanClubChangeHere = canClubChangeHere;
+        }
+
+        public User(string name, bool canSwedishE)
+        {
+            UserName = name;
+            CanSwedishE = canSwedishE;
+        }
+
+
+    }
+}
