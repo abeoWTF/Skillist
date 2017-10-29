15c5f9887855ad09bca80a725a2796f5c1d4b278
Jari Sadinm�ki <abeo@live.se>
2017-10-29 09:41:45 +0100

    Fixed checkboxes, name_Textbox needs to revised.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,14 +26,17 @@ namespace Skillist
         public MainWindow()
         {
             InitializeComponent();
+            DisableCheckBox();
         }
-
+        
        private void name_TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
+       {
             name_TextBox.Clear();
             name_TextBox.Opacity = 100;
-        }
-
+            EnableCheckBox();
+            
+       }
+        //Create user.
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             bool swedishEQ, swedishDQ, germanEQ, germanDQ, norwegianEQ, norwegianDQ, returnP, clubAP, clubCH;
@@ -58,20 +62,75 @@ namespace Skillist
 
             if (name_TextBox.Text != "")
             {
+                
                 users.Add(new User(name_TextBox.Text, swedishEQ, swedishDQ, germanEQ, germanDQ, norwegianEQ,
                     norwegianDQ, returnP, clubAP, clubCH));
                 Users_Listbox.Items.Add(users.LastOrDefault());
 
-                name_TextBox.Clear(); 
-                Equeue_Checkbox.IsChecked = false;
+                
 
+                var list = Users_Listbox.Items.Cast<User>().OrderBy(item => item.UserName).ToList();
+                Users_Listbox.Items.Clear();
 
-
+                foreach (User listItem in list)
+                {
+                    Users_Listbox.Items.Add(listItem);
+                }
+                UncheckClearAll();
+                DisableCheckBox();
+                
             }
+
             else
             {
                 MessageBox.Show("Please enter a valid name.", "No name entered.");
             }
         }
+
+        //Disables the checkboxes.
+        public void DisableCheckBox()
+        {
+            Equeue_Checkbox.IsEnabled = false;
+            Dqueue_Checkbox.IsEnabled = false;
+            Dqueue_De_Checkbox.IsEnabled = false;
+            Equeue_De_Checkbox.IsEnabled = false;
+            Equeue_No_Checkbox.IsEnabled = false;
+            Dqueue_No_Checkbox.IsEnabled = false;
+            Returpost_checkbox.IsEnabled = false;
+            Club_Checkbox.IsEnabled = false;
+            ClubChangeHere_Checkbox.IsEnabled = false;
+            
+        }
+        //Enables the checkboxes.
+        public void EnableCheckBox()
+        {
+            Equeue_Checkbox.IsEnabled = true;
+            Dqueue_Checkbox.IsEnabled = true;
+            Dqueue_De_Checkbox.IsEnabled = true;
+            Equeue_De_Checkbox.IsEnabled = true;
+            Equeue_No_Checkbox.IsEnabled = true;
+            Dqueue_No_Checkbox.IsEnabled = true;
+            Returpost_checkbox.IsEnabled = true;
+            Club_Checkbox.IsEnabled = true;
+            ClubChangeHere_Checkbox.IsEnabled = true;
+        }
+        //Unchecks the checkboxes.
+        public void UncheckClearAll()
+        {
+            name_TextBox.Clear();
+            Equeue_Checkbox.IsChecked = false;
+            Dqueue_Checkbox.IsChecked = false;
+            Equeue_De_Checkbox.IsChecked = false;
+            Dqueue_De_Checkbox.IsChecked = false;
+            Equeue_No_Checkbox.IsChecked = false;
+            Dqueue_No_Checkbox.IsChecked = false;
+            Returpost_checkbox.IsChecked = false;
+            Club_Checkbox.IsChecked = false;
+            ClubChangeHere_Checkbox.IsChecked = false;
+
+        }
+
+
+        
     }
 }
